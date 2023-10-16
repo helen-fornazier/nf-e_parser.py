0e05b068480b13589ec59acedebdad383686f630
Helen Koike <helen.fornazier@gmail.com>
2023-10-16 10:44:20 -0300

    some fixes and tweaks

--- nf-e_parser.py
+++ nf-e_parser.py
@@ -42,7 +42,7 @@ def extract_data_from_xml(file_path):
             'dest_xNome': "",
             'prod_xProd': "",
             'prod_uCom': "",
-            'prod_qCom': 0,
+            'prod_qCom': "",
             'ide_dhEmi': "",
             'arquivo': file_path,
         }
@@ -61,10 +61,66 @@ def extract_data_from_xml(file_path):
         'arquivo': file_path,
     }
 
+def contains_any_substring(var, substrings):
+    var = var.lower()
+    return any(sub in var for sub in substrings)
+
+# CNPJ 22659620000170	SUCATAS BERTASSO LTDA. ME.
+# CNPJ 27051773000234	AMBIENTAL ABELARDI LTDA
+# CNPJ 68108232000100	COMERCIO DE SUCATAS ABELARDI LTDA --------> NAO USAR
+
+def classify_entrada_saida(row):
+    entrada_cnpj = ["22659620000170", "27051773000234", "68108232000100"]
+    if row['emit_CNPJ'] in entrada_cnpj:
+        return 'saida'
+    elif row['emit_CNPJ']:
+        return 'entrada'
+
+def classify_material(row):
+    word_map = {
+        "plastico": ["plast", "pet"],
+        "vidro": ["vidro"],
+        "papel/papelao": ["papel"],
+        "metal": ["metal", "alum", "inox", "ferro", "aco"],
+        "borracha": ["borracha", "pneu"],
+        "madeira": ["madeira", "pallet"],
+    }
+
+    for material, words in word_map.items():
+        if contains_any_substring(row['prod_xProd'], words):
+            return material
+
+    for material, words in word_map.items():
+        if contains_any_substring(row['dest_xNome'], words):
+            return material
+
+def classify_elegibility(row):
+    if not row["tipo"] or row["tipo"] == "desconhecido":
+        return
+
+    allowed_cnpj = ["22659620000170", "27051773000234"]
+    material_eligble = ["plastico", "vidro", "papel/papelao", "metal"]
+    if (row["material"] in material_eligble and row["entrada/saida"] == "saida"
+        and contains_any_substring(row["prod_uCom"], ["kg", "ton"])
+        and row["prod_qCom"] and float(row["prod_qCom"].replace(",", ".")) > 0
+        and row["emit_CNPJ"] in allowed_cnpj
+        and row["ide_dhEmi"][:4] in ["2020", "2021", "2022"]
+        ):
+        return "sim"
+    elif (
+        (row["material"] and row["material"] not in material_eligble)
+        or row["entrada/saida"] == "entrada"
+        or not contains_any_substring(row["prod_uCom"], ["kg", "ton"])
+        or not row["prod_qCom"] or float(row["prod_qCom"].replace(",", ".")) <= 0
+        or row["emit_CNPJ"] not in allowed_cnpj
+        ):
+        return "nao"
+
+
 # Função para listar todos os arquivos xml em um diretório e seus subdiretórios e escrever os dados extraídos no arquivo CSV
 def list_xml_files_in_directory_and_write_to_csv(dir_path, csv_path):
     with open(csv_path, 'w', newline='') as csvfile:
-        fieldnames = ['tipo', 'ide_dhEmi', 'emit_CNPJ', 'emit_xNome', 'dest_CNPJ', 'dest_xNome', 'prod_xProd', 'prod_qCom', 'prod_uCom', 'arquivo']
+        fieldnames = ['elegivel', 'tipo', 'ide_dhEmi', 'emit_CNPJ', 'emit_xNome', 'dest_CNPJ', 'dest_xNome', 'prod_xProd', 'prod_qCom', 'prod_uCom', 'entrada/saida', 'material', 'arquivo']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
 
         writer.writeheader()
@@ -75,6 +131,9 @@ def list_xml_files_in_directory_and_write_to_csv(dir_path, csv_path):
                     file_path = os.path.join(root, file)
                     print(f'Processando arquivo: {file_path}')
                     row = extract_data_from_xml(file_path)
+                    row['entrada/saida'] = classify_entrada_saida(row)
+                    row['material'] = classify_material(row)
+                    row['elegivel'] = classify_elegibility(row)
                     writer.writerow(row)
 
 
